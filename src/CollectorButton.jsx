b2f0a02b582be470f2289c64e9378ef3dff994d8
Chris Wehner <chrisw7128@gmail.com>
2023-05-17 21:25:57 -0700

    first commit

--- src/CollectorButton.jsx
+++ src/CollectorButton.jsx
@@ -1,13 +1,11 @@
 import React, { useContext } from "react";
-
+import StyleContext from "./StyleContext";
 
 export default function CollectorButton() {
-  
+  const buttonStyling = useContext(StyleContext);
   return (
     <div>
-      <button>
-        Click Here!
-      </button>
+      <button style={buttonStyling}>Click Here!</button>
     </div>
   );
 }
